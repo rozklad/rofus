a926dc5ac2bb46235cd0b32bb84eff333fa44461
rozklad <jan.rozklad@gmail.com>
2018-03-03 13:33:31 +0100

    Licensed

--- src/Client.php
+++ src/Client.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @author      bitterend <info@bitterend.io>
+ * @license     http://www.wtfpl.net/
+ *
+ * @link        https://github.com/rozklad/rofus
+ */
+
 
 namespace Rozklad\Rofus;
 
