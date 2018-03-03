4154f5c89187b9ffe75547dcaf5468094c47ab32
rozklad <jan.rozklad@gmail.com>
2018-03-03 13:56:00 +0100

    example start

---
+++ examples/basic/index.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Rofus
+ *
+ * @package  Rozklad/Rofus
+ * @author   bitterend <info@bitterend.io>
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Autoload
+|--------------------------------------------------------------------------
+|
+| Autoload dependencies of the project.
+|
+*/
+
+require __DIR__.'/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run the example
+|--------------------------------------------------------------------------
+|
+| Run sample task for the soap.
+|
+*/
+
+var_dump(class_exists('Rozklad\Rofus\Client'));
